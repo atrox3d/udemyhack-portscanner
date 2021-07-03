8d2847ea139fc44ad60e64b1dc492430dc3b2ac4
atrox3d <atrox3d@gmail.com>
2021-07-03 11:06:43 +0200

    add timeout

--- portscanner.py
+++ portscanner.py
@@ -8,11 +8,11 @@ def scan_port(ipaddress, port):
     try:
         port = int(port)
         sock = socket.socket()
+        sock.settimeout(0.5)
         sock.connect((ipaddress, port))
         print(f'[+] Port {port} is open.')
     except Exception as e:
-        print(f'[-] Port {port} is closed.')
-        print(f'[-] {repr(e)}')
+        print(f'[-] Port {port} is closed, reason: {repr(e)}')
 
 
 if not len(sys.argv) > 1:
@@ -22,9 +22,9 @@ else:
 
 # port = 80
 # port = int(input('[+] Enter port to scan:'))
-ports = range(1, 10)
+ports = range(75, 85)
 print(f"[+] Target ip : {ipaddress}")
 print(f"[+] Port range: {ports}")
 for port in ports:
-    print(f"[+] scanning {ipaddress}:{port}")
+    # print(f"[+] scanning {ipaddress}:{port}")
     scan_port(ipaddress, port)
