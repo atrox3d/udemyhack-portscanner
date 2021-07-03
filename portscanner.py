34aebd29ca64b0a0b31ac402a3b0e38a6484d6e2
atrox3d <atrox3d@gmail.com>
2021-07-03 11:01:51 +0200

    add range and output

--- portscanner.py
+++ portscanner.py
@@ -1,14 +1,30 @@
+import sys
+import getopt
 import socket
 from IPy import IP
 
-ipaddress = input('[+] Enter target to scan:')
-port = int(input('[+] Enter port to scan:'))
-# port = 80
 
-try:
-    sock = socket.socket()
-    sock.connect((ipaddress, port))
-    print(f'[+] Port {port} is open.')
-except Exception as e:
-    print(f'[-] Port {port} is closed.')
-    print(f'[-] {repr(e)}')
+def scan_port(ipaddress, port):
+    try:
+        port = int(port)
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print(f'[+] Port {port} is open.')
+    except Exception as e:
+        print(f'[-] Port {port} is closed.')
+        print(f'[-] {repr(e)}')
+
+
+if not len(sys.argv) > 1:
+    ipaddress = input('[+] Enter target to scan: ')
+else:
+    ipaddress = sys.argv[1]
+
+# port = 80
+# port = int(input('[+] Enter port to scan:'))
+ports = range(1, 10)
+print(f"[+] Target ip : {ipaddress}")
+print(f"[+] Port range: {ports}")
+for port in ports:
+    print(f"[+] scanning {ipaddress}:{port}")
+    scan_port(ipaddress, port)
