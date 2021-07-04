d4c2c070a83857e4d95f2c515ac1ffb184d9ec9b
atrox3d <atrox3d@gmail.com>
2021-07-04 16:27:57 +0200

    implement command line options and parametric logic

--- ipscan.py
+++ ipscan.py
@@ -1,5 +1,33 @@
+import sys
+import getopt
 import portscanner
 
-target = 'youweb.bancobpm.it'
+try:
+    opts, args = getopt.getopt(
+        sys.argv[1:],
+        "ht:r:s:e:"
+    )
+    print(f"{opts=}")
+    print(f"{args=}")
+    # print(f"{sys.argv[1:]=}")
+except getopt.GetoptError as goe:
+    print(repr(goe))
+    exit(1)
 
-portscanner.scan(target)
+timeout = None
+start = None
+end = None
+for opt, arg in opts:
+    if opt in ['-t']:
+        timeout = float(arg)
+    elif opt in ['-s']:
+        start = int(arg)
+    elif opt in ['-e']:
+        end = int(arg)
+
+# target = 'youweb.bancobpm.it'
+targets = args
+kw = dict(start=start, end=end, timeout=timeout)
+kwa = {k: v for k, v in kw.items() if v is not None}
+for target in targets:
+    portscanner.scan(target, **kwa)
