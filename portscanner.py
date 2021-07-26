d23414d59664197aadbb5cc3c694abd39387cf1f
atrox3d <atrox3d@gmail.com>
2021-07-26 08:14:41 +0200

    add comments

--- portscanner.py
+++ portscanner.py
@@ -18,6 +18,7 @@ LOG_DIR = os.path.join(BASE_DIR, "logs")
 
 
 def check_ip(ip):
+    """check ip address or hostname, returns ipaddress on success"""
     try:
         IP(ip)
         return ip
@@ -26,47 +27,68 @@ def check_ip(ip):
 
 
 def get_banner(sock: socket.socket):
+    """reads the first 1024 bytes from socket"""
     return sock.recv(1024)
 
 
-def decode_banner(banner):
+def decode_banner(bytebanner):
+    """decode first 1024 bytes to string"""
     # converted = str(banner, 'ascii')
-    converted = banner.decode()
+    converted = bytebanner.decode()
     return converted
 
 
 def clean_banner(text_banner):
+    """try to remove non printable chars from banner"""
     text_banner = ''.join([c for c in text_banner if c.isprintable()])
     return text_banner
 
 
 def scan_port(ip, port, timeout=0.5, logger=rootlogger):
+    """scans selected port from ip and tries to extract banner"""
+    ip_padding = 15
+    port_padding = 5
+
+    # try to connect
     try:
         port = int(port)
         sock = socket.socket()
         sock.settimeout(timeout)
         sock.connect((ip, port))
-        ip_padding = 15
-        port_padding = 5
+
+        # try to extract banner
         try:
             banner = get_banner(sock)
             text_banner = decode_banner(banner)
 
             decode_warning = False
             if not all(map(str.isprintable, text_banner)):
+                # found non printable chars
                 text_banner = clean_banner(text_banner)
                 decode_warning = True
+
             if not all(map(str.isprintable, text_banner)):
+                # found AGAIN non printable chars
                 logger.critical(
                     "[!] found non-printable inside banner, "
                     "cannot fix, EXITING")
                 return
+
             if decode_warning:
+                # assume the banner is not normal text
+                print(
+                    termcolor.colored(
+                        f'[+] Open Port {ip:{ip_padding}} {port:>{port_padding}}: '
+                        f'banner (binary)       : *b{{{text_banner}}}.'
+                        'yellow'
+                    )
+                )
                 logger.info(
                     f'[+] Open Port {ip:{ip_padding}} {port:>{port_padding}}: '
                     f'banner (binary)       : *b{{{text_banner}}}.'
                 )
             else:
+                # banner is text
                 print(
                     termcolor.colored(
                         f'[+] Open Port {ip:{ip_padding}} {port:>{port_padding}}: '
@@ -75,9 +97,9 @@ def scan_port(ip, port, timeout=0.5, logger=rootlogger):
                     )
                 )
                 logger.info(
-                        f'[+] Open Port {ip:{ip_padding}} {port:>{port_padding}}: '
-                        f'banner (text)         : *t[{text_banner}].'
-                    )
+                    f'[+] Open Port {ip:{ip_padding}} {port:>{port_padding}}: '
+                    f'banner (text)         : *t[{text_banner}].'
+                )
         except (
                 socket.timeout,
                 ConnectionError,
@@ -85,6 +107,7 @@ def scan_port(ip, port, timeout=0.5, logger=rootlogger):
                 ConnectionAbortedError,
                 ConnectionRefusedError,
         ):
+            # port is open but failed to extract banner
             print(
                 termcolor.colored(
                     f'[+] XOpen Port {ip:{ip_padding}} {port:>{port_padding}}. ',
@@ -97,6 +120,7 @@ def scan_port(ip, port, timeout=0.5, logger=rootlogger):
                 )
             )
         except Exception as e:
+            # unknown error receving banner
             logger.info(
                 f'[+] Open Port {ip:{ip_padding}} {port:>{port_padding}}: '
                 f'error receiving banner: {type(e)} "{e}".'
@@ -107,11 +131,15 @@ def scan_port(ip, port, timeout=0.5, logger=rootlogger):
 
 
 def scan_ports(converted_ip, ports, timeout, threaded=True, logger=rootlogger):
+    """scan multiple ports for ip threaed or not"""
+
     if threaded:
+        # threaded loop
+        threads = []
+        thread_count = 0
+
         try:
             print(f"[+] starting port threads for {converted_ip} {ports}")
-            threads = []
-            thread_count = 0
             for port in ports:
                 th = threading.Thread(
                     target=scan_port,
@@ -119,12 +147,12 @@ def scan_ports(converted_ip, ports, timeout, threaded=True, logger=rootlogger):
                     args=(converted_ip, port, timeout),
                     kwargs=dict(logger=logger)
                 )
-                # print(f"[+] starting thread {th.getName()}")
                 print(f"[+] starting thread {th.getName()}")
                 th.start()
                 threads.append(th)
                 time.sleep(0.5)
 
+            # join loop
             print(f"[+] waiting port threads for {converted_ip}:{ports}")
             for th in threads:
                 print(f"[+] joining thread {th.getName()}")
@@ -134,36 +162,49 @@ def scan_ports(converted_ip, ports, timeout, threaded=True, logger=rootlogger):
             print(f"{thread_count=}")
             return
     else:
+        # non threaded loop
         for port in ports:
             scan_port(converted_ip, port, timeout, logger=logger)
 
 
 def scan_target(target, start=1, end=100, timeout=0.5, port_threading=True, logdir=LOG_DIR):
+    """scan single ip"""
+
+    # setup logger
     logger = logging.getLogger(target)
     logger.setLevel(logging.NOTSET)
-
+    # setup file handler
     logfile = os.path.join(logdir, f"{target}.log")
     filehandler = logging.FileHandler(logfile, mode="w")
     logger.addHandler(filehandler)
+    # setup stream handler
     logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
+    # get ip
     try:
         converted_ip = check_ip(target)
     except Exception as e:
         logger.error(f"[-] invalid address/domain: {target}")
         return False
+
+    # select port range
     ports = range(start, end + 1)
 
+    # show process params
     logger.info(f"[+] Target    : {target}")
     logger.info(f"[+] Target ip : {converted_ip}")
     logger.info(f"[+] Port range: {start}-{end}")
     logger.info(f"[+] timeout   : {timeout}")
 
+    # scan ports
     scan_ports(converted_ip, ports, timeout, port_threading, logger=rootlogger)
     return True
 
 
 def scan_targets(*targets, start, end, timeout, target_threading, port_threading):
+    """scan multuiple ips"""
+
+    # extract kwargs for scan_target function
     kwargs = dict(
         start=start,
         end=end,
@@ -172,10 +213,11 @@ def scan_targets(*targets, start, end, timeout, target_threading, port_threading
     )
 
     if target_threading:
-        try:
-            threads = []
-            thread_count = 0
+        threads = []
+        thread_count = 0
 
+        try:
+            # thread loop
             for target in targets:
                 th = threading.Thread(
                     target=scan_target,
@@ -189,15 +231,18 @@ def scan_targets(*targets, start, end, timeout, target_threading, port_threading
                 threads.append(th)
                 time.sleep(0.5)
 
+            # join loop
             for th in threads:
                 print(f"[+] joining target thread {th.getName()}")
                 th.join()
             print(f"[+] waiting for target threads")
+
         except Exception as e:
             print(repr(e))
             print(f"{thread_count=}")
             return
     else:
+        # non threaded loop
         for target in targets:
             scan_target(target, **kwargs)
 
